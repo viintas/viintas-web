52e97705f34e619d2317baf1e8d008d40ba71d42
Kevin Arias <kevin.arias@viintas.co>
2022-03-16 19:54:38 -0500

    Test

--- src/app/core/products/products-routing.module.ts
+++ src/app/core/products/products-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateProductComponent } from './create-product/create-product.component';
 
 import { ListProductsComponent } from './list-products/list-products.component';
 
 const routes: Routes = [
     {   path: 'list',   component: ListProductsComponent   },
+    {   path: 'create',   component: CreateProductComponent   },
   ];
 
 @NgModule({
