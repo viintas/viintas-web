4254d97d0217453612df35c0b1b36c267d67a984
Juan Sebastian Paz <juan.paz@viintas.co>
2022-09-20 14:04:57 -0500

    Change of logo and images

--- src/app/core/components/information-section/information-section.component.ts
+++ src/app/core/components/information-section/information-section.component.ts
@@ -14,17 +14,17 @@ export class InformationSectionComponent implements OnInit {
   ngOnInit(): void {
     this.information = [
       {
-        img:'./../../../../assets/img/wholesale.svg',
+        img:'./../../../../assets/img/undraw_ideas_flow_re_bmea.svg',
         title:'Ventas al por mayor',
         paragraph:'Compra más de 6 productos y recibe el precio al por mayor'
       },
       {
-        img:'./../../../../assets/img/entrepreneurs.svg',
+        img:'./../../../../assets/img/undraw_mobile_interface_re_1vv9.svg',
         title:'New feactures',
         paragraph:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta animi autem'
       },
       {
-        img:'./../../../../assets/img/productToWeek.svg',
+        img:'./../../../../assets/img/undraw_reading_time_re_phf7.svg',
         title:'Productos Especiales',
         paragraph:'Todas las semanas será seleccionado un nuevo producto con aun más descuento'
       }
